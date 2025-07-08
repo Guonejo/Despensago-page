3eff53f019b112a18ad19e25e613e6da348dc178
Guonejo <82884497+Guonejo@users.noreply.github.com>
2025-07-08 00:52:55 -0400

    c3

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Despensago-page/', // ← agrega esta línea
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
